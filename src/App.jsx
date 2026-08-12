d17076d3b961f7b51afa9041895d84e0895ec266
colonius23 <colonius@gmail.com>
2026-08-12 12:50:08 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import BloodTests from './pages/BloodTests'
 import Activities from './pages/Activities'
 import Sleep from './pages/Sleep'
 import Vitals from './pages/Vitals'
+import Report from './pages/Report'
 import Upload from './pages/Upload'
 import Settings from './pages/Settings'
 
@@ -12,7 +13,9 @@ export default function App() {
   return (
     <HashRouter>
       <div className="flex min-h-screen">
-        <Sidebar />
+        <div className="print:hidden">
+          <Sidebar />
+        </div>
         <main className="flex-1 p-6 md:p-8 max-w-6xl">
           <Routes>
             <Route path="/" element={<Dashboard />} />
@@ -20,6 +23,7 @@ export default function App() {
             <Route path="/activities" element={<Activities />} />
             <Route path="/sleep" element={<Sleep />} />
             <Route path="/vitals" element={<Vitals />} />
+            <Route path="/report" element={<Report />} />
             <Route path="/upload" element={<Upload />} />
             <Route path="/settings" element={<Settings />} />
           </Routes>
